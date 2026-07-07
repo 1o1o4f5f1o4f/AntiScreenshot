cb8206ee30cb87358aa3508afefe9a6c78259bfd
asewoirew <asewoirew@noreply.gitcode.com>
2026-07-07 22:16:54 +0800

    格式化

--- make.bat
+++ make.bat
@@ -1,8 +1,11 @@
 @echo off
 if not exist build mkdir build
+
 echo ----- Building resources -----
 windres src\Resource.rc -o build\Resource.o || exit /b 1
+
 echo ----- Building executable -----
 g++ src\main.cpp build\Resource.o -o build\main.exe -mwindows -static || exit /b 1
+
 echo ----- Running executable -----
 build\main.exe

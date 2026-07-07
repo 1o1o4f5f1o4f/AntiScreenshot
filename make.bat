2558fed9999b22595f8c3ef9b057e15ca90776ae
asewoirew <asewoirew@noreply.gitcode.com>
2026-07-07 22:19:16 +0800

    修改编译参数

--- make.bat
+++ make.bat
@@ -5,7 +5,7 @@ echo ----- Building resources -----
 windres src\Resource.rc -o build\Resource.o || exit /b 1
 
 echo ----- Building executable -----
-g++ src\main.cpp build\Resource.o -o build\main.exe -mwindows -static || exit /b 1
+g++ src\main.cpp build\Resource.o -o build\antiscreenshot.exe -mwindows -static -Wall -Oz || exit /b 1
 
 echo ----- Running executable -----
-build\main.exe
+build\antiscreenshot.exe
